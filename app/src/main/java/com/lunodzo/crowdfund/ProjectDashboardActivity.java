b9a74e63cbef0c2d3c224fa6dbb0a6a396df2985
Lunodzo Mwinuka <lunomwinuka@gmail.com>
2020-09-19 19:19:02 +0300

    Presented version 2

--- app/src/main/java/com/lunodzo/crowdfund/ProjectDashboardActivity.java
+++ app/src/main/java/com/lunodzo/crowdfund/ProjectDashboardActivity.java
@@ -2,7 +2,6 @@ package com.lunodzo.crowdfund;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -16,13 +15,15 @@ public class ProjectDashboardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_project_dashboard);
 
         Button button = findViewById(R.id.invest);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent moveIntent = new Intent(getApplicationContext(), ProjectsActivity.class);
-                Toast.makeText(getApplicationContext(), "Coming soon", Toast.LENGTH_LONG).show();
-                startActivity(moveIntent);
-            }
+        button.setOnClickListener(view -> {
+            Intent moveIntent = new Intent(getApplicationContext(), ProjectsActivity.class);
+            Toast.makeText(getApplicationContext(), "Coming soon", Toast.LENGTH_LONG).show();
+            startActivity(moveIntent);
         });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
